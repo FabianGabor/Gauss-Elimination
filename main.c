01cf848230d28cb9e534d1cf29c2d0e59c0ef1e7
Fábián Gábor <info@fabiangabor.com>
2019-11-20 12:35:51 +0100

    b matrix számítása

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-void print_matrix(double *a, int n)
+void print_matrix(double *a, double *b, int n)
 {
     for (int i=0; i<n; i++)
     {
         for (int j=0; j<n; j++)
             printf("%5.1f", *(a + i*n + j ));
+        printf("%5.1f", *(b + i));
         printf("\n");
     }
     printf("\n");
@@ -24,9 +25,9 @@ int main()
         {2,3,1},
         {2,4,-3}
     };
-    int b[3] = {8,1,3};
+    double b[3] = {2,5,2};
 
-    print_matrix(*a,3);
+    print_matrix(*a, b, 3);
 
     int n=3;
     for (int k=0; k<n-1; k++)
@@ -35,6 +36,11 @@ int main()
         {
             double mult = a[i][k];
             double lead = a[k][k];
+
+            b[i] *= lead;
+            b[i] -= mult * b[k];
+            //b[i] -= mult / lead * b[k];
+
             for (int j=k; j<n; j++)
             {
                 a[i][j] *= lead;
@@ -43,7 +49,7 @@ int main()
         }
     }
 
-    print_matrix(*a,3);
+    print_matrix(*a, b, 3);
 
     return 0;
 }

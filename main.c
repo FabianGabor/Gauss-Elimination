da9cc9ffbedef80e6b8a01f983e97eae079d847b
Fábián Gábor <info@fabiangabor.com>
2019-11-20 17:38:16 +0100

    cseréld az első sort, ha a[1][1] = 0

--- main.c
+++ main.c
@@ -12,21 +12,35 @@ void print_matrix(int *a, int *b, int n)
     printf("\n");
 }
 
+int *swap(int *a, int *b, int n)
+{
+    for (int i=0; i<n; i++)
+    {
+        int tmp_val = *(a + i);
+        *(a + i) = *(a + n + i);
+        *(a + n + i) = tmp_val;
+
+    }
+    return a;
+}
+
 int main()
 {
     int a[4][4] =
     {
-        {1,2,5,1},
+        {0,2,5,1},
         {3,-4,3,-2},
         {4,3,2,-1},
         {1,-2,-4,-1}
     };
     int b[4] = {4,7,1,2};
-    int x[4] = {0};
+    double x[4] = {0};
 
     int n = sizeof(b)/sizeof(b[0]);
 
-    print_matrix(*a, b, n);
+    if (a[0][0] == 0) swap(*a,b,n);
+
+    print_matrix(a, b, n);
 
 
     for (int k=0; k<n-1; k++)
@@ -52,7 +66,7 @@ int main()
     for (int i=n-1; i>=0; i--)
     {
         int j;
-        int sum = b[i];
+        double sum = b[i];
         for (j=0; j<n; j++)
         {
             sum -= a[i][j] * x[j];
@@ -61,7 +75,7 @@ int main()
     }
 
     for (int i=0; i<n; i++)
-        printf("x%d = %3d\n", i+1, x[i]);
+        printf("x%d = %7.3f\n", i+1, x[i]);
 
     return 0;
 }
